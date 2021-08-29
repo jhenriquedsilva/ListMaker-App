de4873adaf703082c605ee12a70155a0ea4aad0a
jhenriquedsilva <josehs555@gmail.com>
2021-08-29 10:25:32 -0300

    Adding tasks to the list

--- app/src/main/java/com/raywenderlich/listmaker/ui/main/detail/ListDetailActivity.kt
+++ app/src/main/java/com/raywenderlich/listmaker/ui/main/detail/ListDetailActivity.kt
@@ -2,7 +2,10 @@ package com.raywenderlich.listmaker.ui.main.detail
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.InputType
 import android.view.LayoutInflater
+import android.widget.EditText
+import androidx.appcompat.app.AlertDialog
 import androidx.lifecycle.ViewModelProvider
 import com.raywenderlich.listmaker.MainActivity
 import com.raywenderlich.listmaker.R
@@ -37,4 +40,23 @@ class ListDetailActivity : AppCompatActivity() {
                 .commitNow()
         }
     }
+
+    private fun showCreateTaskDialog() {
+        val taskEditText = EditText(this)
+        taskEditText.inputType = InputType.TYPE_CLASS_TEXT
+
+        AlertDialog.Builder(this)
+            .setTitle(R.string.task_to_add)
+            .setView(taskEditText)
+            .setPositiveButton(R.string.add_task) { dialog, _ ->
+                val task = taskEditText.text.toString()
+
+                viewModel.addTask(task)
+
+                dialog.dismiss()
+            }
+            .create()
+            .show()
+    }
 }
+
--- app/src/main/java/com/raywenderlich/listmaker/ui/main/detail/ui/detail/ListDetailFragment.kt
+++ app/src/main/java/com/raywenderlich/listmaker/ui/main/detail/ui/detail/ListDetailFragment.kt
@@ -6,6 +6,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.raywenderlich.listmaker.R
 import com.raywenderlich.listmaker.databinding.ListDetailFragmentBinding
 
@@ -27,7 +28,13 @@ class ListDetailFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(requireActivity()).get(ListDetailViewModel::class.java)
-        // TODO: Use the ViewModel
+        val recyclerAdapter = ListItemsRecyclerViewAdapter(viewModel.list)
+        binding.listItemRecyclerview.adapter = recyclerAdapter
+        binding.listItemRecyclerview.layoutManager = LinearLayoutManager(requireContext())
+
+        // Inside the lambda, the adapter is notified that the list of tasks has updated. This
+        // causes the RecyclerView to be redrawn, showing any new items
+        viewModel.onTaskAdded = { recyclerAdapter.notifyDataSetChanged() }
     }
 
 }
